826ac67ac9443ed0e2bcbbbea639b6b47d2d5415
emreertugrl <emrertugrl@gmail.com>
2024-10-13 20:16:43 +0300

    app.js files way  changed into src folder

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import CustomText from './utils/CustomText';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <CustomText>emre</CustomText>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
